2db4840c765a5ed194e88d3ab849d3d0c8540440
sherif-elshamaa <88216982+sherif-elshamaa@users.noreply.github.com>
2021-08-18 21:32:32 +0200

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,7 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://thesocialmedia.vercel.app";
+    : "https://thesocialmedia.herokuapp.com";
 
 
 export default baseUrl;
