3cbfc90a12bce45277f506e883e77cded56ff190
sherif-elshamaa <sherif.elshamaaa@gmail.com>
2021-08-18 18:58:20 +0200

    first commit

---
+++ components/Notifications/CommentNotification.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Feed } from "semantic-ui-react";
+import calculateTime from "../../utils/calculateTime";
+import Link from "next/link";
+
+function CommentNotification({ notification }) {
+  return (
+    <>
+      <Feed.Event>
+        <Feed.Label image={notification.user.profilePicUrl} />
+        <Feed.Content>
+          <Feed.Summary>
+            <>
+              <Feed.User as="a" href={`/${notification.user.username}`}>
+                {notification.user.name}
+              </Feed.User>{" "}
+              Commented on Your <Link href={`/post/${notification.post._id}`}>post</Link>
+              <Feed.Date>{calculateTime(notification.date)}</Feed.Date>
+            </>
+          </Feed.Summary>
+          {notification.post.picUrl && (
+            <Feed.Extra images>
+              <Link href={`/post/${notification.post._id}`}>
+                <img src={notification.post.picUrl} />
+              </Link>
+            </Feed.Extra>
+          )}
+          <Feed.Extra text>
+            <strong>{notification.text}</strong>
+          </Feed.Extra>
+        </Feed.Content>
+      </Feed.Event>
+      <br />
+    </>
+  );
+}
+
+export default CommentNotification;
