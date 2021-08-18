69f33d0917db09967b58a91d5446b3003883b27c
sherif-elshamaa <88216982+sherif-elshamaa@users.noreply.github.com>
2021-08-18 20:02:45 +0200

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "thesocialmediaa.netlify.app";
+    : "https://sherif-elshamaa.github.io/thesocialmedia";
 
 export default baseUrl;
