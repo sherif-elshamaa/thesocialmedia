275c10a0852519d35bc33af6e8e9fbae6ba2a5d4
sherif-elshamaa <88216982+sherif-elshamaa@users.noreply.github.com>
2021-08-18 20:43:54 +0200

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://thesocialmedia-g259pt3wr-thesocialmedia.vercel.app";
+    : "https://thesocialmedia.vercel.app";
 
 export default baseUrl;
