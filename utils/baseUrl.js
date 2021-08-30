40c4716f92d28887a0b26317047df94b69c6b447
sherif-elshamaa <sherif.elshamaaa@gmail.com>
2021-08-30 22:09:01 +0200

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://thesocialmediaa.web.app";
+    : "https://thesocialmedia.herokuapp.com";
 
 module.exports = baseUrl;
